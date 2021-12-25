56e9a1105e1c0e2d326dd4db14d9c9eee8c0c2e0
Pascal Blessing <pascal.blessing@gmx.at>
2021-12-25 15:21:16 +0100

    Add day 14

---
+++ day14-1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+string[] lines = File.ReadAllLines("input.txt");
+
+var initialPolymer = lines.First();
+
+var instructions = lines
+    .Skip(2)
+    .Select(x => Regex.Match(x, @"([A-Z])([A-Z]) -> ([A-Z])"))
+    .ToDictionary(
+        x => (x.Groups[1].Value[0], x.Groups[2].Value[0]),
+        x => x.Groups[3].Value[0]
+    );
+
+int steps = 10;
+
+var currentPolymer = initialPolymer;
+
+for(int i = 0; i < steps; i++)
+{
+    StringBuilder sb = new StringBuilder();
+    for(int charIndex = 0; charIndex < currentPolymer.Length - 1; charIndex++)
+    {
+        sb.Append(currentPolymer[charIndex]);
+        sb.Append(instructions[(currentPolymer[charIndex], currentPolymer[charIndex+1])]);
+    }
+    sb.Append(currentPolymer[currentPolymer.Length - 1]);
+
+    currentPolymer = sb.ToString();
+}
+
+var groups = currentPolymer.GroupBy(x => x).Select(x => x.Count()).ToArray();
+
+var max = groups.Max();
+var min = groups.Min();
+
+Console.WriteLine(max - min);
---
+++ day14-2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+string[] lines = File.ReadAllLines("input.txt");
+
+var initialPolymer = lines.First();
+
+var instructions = lines
+    .Skip(2)
+    .Select(x => Regex.Match(x, @"([A-Z])([A-Z]) -> ([A-Z])"))
+    .ToDictionary(
+        x => (x.Groups[1].Value[0], x.Groups[2].Value[0]),
+        x => x.Groups[3].Value[0]
+    );
+
+int steps = 40;
+
+var polymerPairs = instructions.ToDictionary(x => x.Key, x => (ulong)0);
+
+
+for(int charIndex = 0; charIndex < initialPolymer.Length - 1; charIndex++)
+{
+    polymerPairs[(initialPolymer[charIndex], initialPolymer[charIndex+1])]++;
+}
+
+for(int i = 0; i < steps; i++)
+{
+    var newPolymerPairs = instructions.ToDictionary(x => x.Key, x => (ulong)0);
+
+    foreach(var kv in polymerPairs)
+    {
+        char newChar = instructions[kv.Key];
+        newPolymerPairs[(kv.Key.Item1, newChar)] += kv.Value;
+        newPolymerPairs[(newChar, kv.Key.Item2)] += kv.Value;
+    }
+
+    polymerPairs = newPolymerPairs;
+}
+
+var charCounts = new Dictionary<char, ulong>();
+
+foreach(var kv in polymerPairs)
+{
+    if(!charCounts.ContainsKey(kv.Key.Item1)) charCounts.Add(kv.Key.Item1, (ulong) 0);
+
+    charCounts[kv.Key.Item1] += kv.Value;
+}
+
+if(!charCounts.ContainsKey(initialPolymer.Last())) charCounts.Add(initialPolymer.Last(), (ulong) 0);
+
+charCounts[initialPolymer.Last()]++;
+
+var max = charCounts.Values.Max();
+var min = charCounts.Values.Min();
+
+Console.WriteLine(max - min);
