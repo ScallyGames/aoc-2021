5be3ac47bb7682887e0c3040c19d1ebf464ce282
Pascal Blessing <pascal.blessing@gmx.at>
2021-12-25 15:21:01 +0100

    Add day 12

---
+++ day12-1/Program.cs
@@ -0,0 +1,32 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+
+Dictionary<string, Cave> caves = new Dictionary<string, Cave>();
+
+
+// Transform connections into cave graph
+foreach(var line in lines)
+{
+    var connection = line.Split('-');
+
+    for(int i = 0; i < connection.Length; i++)
+    {
+        if(!caves.ContainsKey(connection[i]))
+        {
+            caves.Add(connection[i], new Cave(connection[i]));
+        }
+    }
+
+    for(int i = 0; i < connection.Length; i++)
+    {
+        caves[connection[i]].AdjacentCaves.Add(caves[connection[connection.Length - i - 1]]);
+    }
+}
+
+var paths = caves["start"].GetAllPathsTo("end", new List<Cave>());
+
+Console.WriteLine(paths.Count);
+
+// foreach(var path in paths)
+// {
+//     Console.WriteLine(string.Join(',', path.Select(x => x.Name)));
+// }
---
+++ day12-2/Cave.cs
@@ -0,0 +1,44 @@
+class Cave
+{
+    public string Name { get; private set; }
+
+    public List<Cave> AdjacentCaves = new List<Cave>();
+
+    public Cave(string name)
+    {
+        this.Name = name;
+    }
+
+    public bool IsLargeCave => this.Name.ToLower() != this.Name;
+
+    public List<List<Cave>> GetAllPathsTo(string targetName, List<Cave> previousPath)
+    {
+        previousPath.Add(this);
+
+        List<List<Cave>> paths = new List<List<Cave>>();
+
+        if(this.Name == targetName)
+        {
+            paths.Add(previousPath);
+            return paths;
+        }
+
+        var pathCandidates = this.AdjacentCaves.Where(x => IsValidCandiateForPath(x, previousPath));
+
+        foreach(var candidate in pathCandidates)
+        {
+            List<Cave> newPath = new List<Cave>(previousPath);
+
+            paths.AddRange(candidate.GetAllPathsTo(targetName, newPath));
+        }
+
+        return paths;
+    }
+
+    private bool IsValidCandiateForPath(Cave candidate, List<Cave> previousPath)
+    {
+        if(candidate.Name == previousPath[0].Name) return false;
+
+        return candidate.IsLargeCave || !previousPath.Contains(candidate) || (!previousPath.GroupBy(x => x).Any(x => !x.Key.IsLargeCave && x.Count() > 1));
+    }
+}
