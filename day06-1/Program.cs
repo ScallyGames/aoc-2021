602c3a9e70aa2d76f5c196d5afc4f30d68baa741
Pascal Blessing <pascal.blessing@gmx.at>
2021-12-25 15:19:31 +0100

    Add day 6

---
+++ day06-1/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+
+List<int> lanternfish = lines[0].Split(',').Select(x => int.Parse(x)).ToList();
+
+int numberOfDays = 80;
+
+while(numberOfDays > 0)
+{
+    List<int> newLanternfish = new List<int>();
+
+    for(int i = 0; i < lanternfish.Count; i++)
+    {
+        lanternfish[i]--;
+        if(lanternfish[i] < 0)
+        {
+            lanternfish[i] = 6;
+            newLanternfish.Add(8);
+        }
+    }
+    lanternfish.AddRange(newLanternfish);
+
+    numberOfDays--;
+}
+
+Console.WriteLine(lanternfish.Count);
---
+++ day06-2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Numerics;
+
+string[] lines = File.ReadAllLines("input.txt");
+
+var lanternfish = lines[0].Split(',').Select(x => int.Parse(x)).GroupBy(x => x).ToDictionary(x => x.Key, x => new BigInteger(x.Count()));
+
+for(int i = 0; i <= 8; i++)
+{
+    if(!lanternfish.ContainsKey(i))
+    {
+        lanternfish.Add(i, new BigInteger(0));
+    }
+}
+
+int numberOfDays = 256;
+
+while(numberOfDays > 0)
+{
+    Console.WriteLine("New day");
+    for(int i = 0; i <= 8; i++)
+    {
+        Console.WriteLine(i + " " + lanternfish[i]);
+    }
+    Console.WriteLine("");
+    BigInteger fishesGivingBirth = lanternfish[0];
+    
+    for(int i = 0; i < 8; i++)
+    {
+        lanternfish[i] = lanternfish[i+1];
+    }
+
+    lanternfish[6] += fishesGivingBirth;
+    lanternfish[8] = fishesGivingBirth;
+
+    numberOfDays--;
+}
+
+Console.WriteLine(lanternfish.Aggregate(new BigInteger(0), (a, b) => a + b.Value));
